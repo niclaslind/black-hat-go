0955b7387f71a9d9b779e3b039369b6b56c56a9e
Niclas <niclas.lind93@gmail.com>
2021-05-14 11:59:48 +0200

    added aes and hmac

---
+++ ch11/aes/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"errors"
+)
+
+func unpad(buf []byte) []byte {
+	// Assume valid length and padding, Should add checks
+	padding := int(buf[len(buf)-1])
+	return buf[:len(buf)-padding]
+}
+
+func decrypt(ciphertext, key []byte) ([]byte, error) {
+	var (
+		plaintext []byte
+		iv        []byte
+		block     cipher.Block
+		mode      cipher.BlockMode
+		err       error
+	)
+
+	if len(ciphertext) < aes.BlockSize {
+		return nil, errors.New("Invalid ciphertext lenth: too short")
+	}
+
+	if len(ciphertext)%aes.BlockSize != 0 {
+		return nil, errors.New("Invalid ciphertext length: not a multiple of blocksize")
+	}
+
+	iv = ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	if block, err = aes.NewCipher(key); err != nil {
+		return nil, err
+	}
+
+	mode = cipher.NewCBCDecrypter(block, iv)
+	plaintext = make([]byte, len(ciphertext))
+	mode.CryptBlocks(plaintext, ciphertext)
+	plaintext = unpad(plaintext)
+
+	return plaintext, nil
+}
